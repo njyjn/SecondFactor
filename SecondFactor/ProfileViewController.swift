963e8a6af49a8698017d7e9889d96099925ebf39
njyjn <justinn@u.nus.edu>
2017-09-23 18:23:03 -0700

    feature: added profile screen and associated logic

--- SecondFactor/ProfileViewController.swift
+++ SecondFactor/ProfileViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBAction func logout(_ sender: UIButton) {
+        //removing values from default
+        UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+        UserDefaults.standard.synchronize()
+        
+        //switching to login screen
+        let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        self.navigationController?.pushViewController(loginViewController, animated: true)
+        self.dismiss(animated: false, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //hiding back button
+        let backButton = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: navigationController, action: nil)
+        navigationItem.leftBarButtonItem = backButton
+        
+        //getting user data from defaults
+        let defaultValues = UserDefaults.standard
+        if let name = defaultValues.string(forKey: "username"){
+            //setting the name to label
+            usernameLabel.text = name
+        }else{
+            //send back to login view controller
+        }
     }
     
     override func didReceiveMemoryWarning() {
--- SecondFactor/ViewController.swift
+++ SecondFactor/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    @IBOutlet weak var appnameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //the defaultvalues to store user data
+    let defaultValues = UserDefaults.standard
+    
+    // MARK: Actions
+    @IBAction func login(_ sender: UIButton) {
+        let parameters: Parameters=[
+            "username":usernameTextField.text!,
+            "password":passwordTextField.text!
+        ]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //hiding the navigation button
+        let backButton = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: navigationController, action: nil)
+        navigationItem.leftBarButtonItem = backButton
+
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //if user is already logged in switching to profile screen
+        if defaultValues.string(forKey: "username") != nil{
+            let profileViewController = self.storyboard?.instantiateViewController(withIdentifier: "ProfileViewcontroller") as! ProfileViewController
+            self.navigationController?.pushViewController(profileViewController, animated: true)
+            
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
