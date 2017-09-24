ddebecbbf02c14eb0140288d40b22f5d7433f444
njyjn <justinn@u.nus.edu>
2017-09-23 18:19:25 -0700

    infra: added Alamofire cocoapod for HTTP

---
+++ SecondFactor/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  SecondFactor
+//
+//  Created by Justin on 9/23/17.
+//  Copyright © 2017 Aletheon Corp. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
